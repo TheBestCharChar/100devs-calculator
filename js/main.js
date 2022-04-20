c08a89cd719186fdc220c65dc0a175aed3bada86
Christopher Jin <chrisjin1@gmail.com>
2022-04-19 22:09:41 -0400

    html + Css, OOP in progress

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// ? Click number buttons
+// ? Numbers are stored and shown on display
+// ? store operators
+// ? Number Operands are also stored and shown on display
+// ? calculations are made
+// ? return a result
+// ? result is displayed on screen
+// ? clear button should clear display and storage
+// ? prevent invalid inputs
+// https://theodorusandi.hashnode.dev/build-a-calculator-using-4-pillars-of-object-oriented-programming-in-javascript#heading-javascript
+
+
+function Calculator() {
+    this.a = 0
+    this.b = 0
+}
